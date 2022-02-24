d38a20979f876867a49f420b3aced502e504b917
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-02-24 17:17:36 -0300

    cleaning main class

--- src/Program.java
+++ src/Program.java
@@ -14,26 +14,15 @@ import static Logos.utils.CsvReader.readCsvCategories;
 public class Program {
     public static void main(String[] args) throws IOException {
 
-//        List<SubCategory> subCategories = toReadCsvToSubCategories("/home/kelly/Downloads/planilha-dados-escola - Subcategoria.csv", categories);
-//        List<Course> courses = toReadCsvTocourses("/home/kelly/Downloads/planilha-dados-escola - Curso.csv", subCategories);
-//        Collections.sort(categories, Comparator.comparing(Category::getOrder));
-//        Collections.sort(subCategories, Comparator.comparing(SubCategory::getOrder));
-
-//      System.out.println(categories);
-//      System.out.println(subCategories);
-//      System.out.println(courses);
-
-
         List<Category> categories = readCsvCategories("/home/kelly/Downloads/planilha-dados-escola - Categoria.csv");
         List<SubCategory> subCategories = readCsvSubCategories("/home/kelly/Downloads/planilha-dados-escola - Subcategoria.csv", categories);
         List<Course> courses = readCsvCourses("/home/kelly/Downloads/planilha-dados-escola - Curso.csv", subCategories);
 
+        System.out.println(categories);
+        System.out.println(subCategories);
         System.out.println(courses);
 
-        toGenerateHtml(courses,subCategories,categories);
-
-
-
+        toGenerateHtml(courses, subCategories, categories);
 
 
     }
