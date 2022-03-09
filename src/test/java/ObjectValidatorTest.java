169af5763e253e1b37154cd7e240bc890ea61b42
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-09 19:39:18 -0300

    ObjectValidatorTest test ready for next review

---
+++ src/test/java/ObjectValidatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static Logos.commonValidator.ObjectValidator.isObjectValid;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ObjectValidatorTest {
+    @Test
+    void isObjectValidShouldThrowIllegalArgumentExceptionIfObjectIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> isObjectValid(null, "Não pode ser null"));
+    }
+
+    @Test
+    void isObjectValidShouldNotThrowExceptionIfObjectIsNotNull() {
+        assertDoesNotThrow(() -> isObjectValid("Java", ""));
+    }
+}
