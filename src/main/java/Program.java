5121e96cff764b80529d74b044f45fdaaa13a0b1
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-09 23:01:32 -0300

    CourseServiceTest is ready for next review

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,16 +1,24 @@
 import Logos.category.Category;
+import Logos.course.Course;
+import Logos.subCategory.SubCategory;
 
+import java.util.List;
+
+import static Logos.course.CourseService.getSubCategoryName;
+import static Logos.utils.CsvReader.*;
 import static Logos.utils.GenerateSql.writeSql;
 
 public class Program {
     public static void main(String[] args) {
-        /*writeSql();
+       writeSql();
+        List<Category> categories = readCategories("files/planilha-dados-escola - Categoria.csv");
+        List<SubCategory> subCategories = readSubCategories("files/planilha-dados-escola - Subcategoria.csv",categories);
+        List<Course> courses = readCourses("files/planilha-dados-escola - Curso.csv",subCategories);
+
+        System.out.println(getSubCategoryName(courses));
         //TODO testar os validadores e o service de course
         //TODO Se der tempo testar
-       */
-        String code ="builds-e-controle-de-versao";
-        Category category = new Category("programacao","programacao");
-        System.out.println(code.matches("[a-z-]+")); ;
-        System.out.println(category);
+
+
     }
 }
---
+++ src/test/java/CourseServiceTest.java
@@ -0,0 +1,48 @@
+import Logos.category.Category;
+import Logos.course.Course;
+import Logos.subCategory.SubCategory;
+import Logos.subCategory.enums.SubCategoryStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static Logos.course.CourseService.getSubCategoryName;
+import static Logos.course.CourseService.getTotalCourseHours;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CourseServiceTest {
+
+    private static List<Course> courses;
+    private static Category category;
+    private static SubCategory subCategory;
+
+    @BeforeEach
+    private void setUp() {
+        category = new Category("programação", "programacao");
+
+        subCategory = new SubCategory("java", "java", "java", SubCategoryStatus.ACTIVE, 2, category);
+
+        courses = Arrays.asList(
+                new Course("java2", "java2", 10, true, "Iniciantes",
+                        "Paulo", "Curso de Java", "java", subCategory),
+                new Course("java3", "java3", 10, true, "Iniciantes",
+                        "Camila", "Curso de Java", "java", subCategory),
+                new Course("java4", "java4", 10, true, "Iniciantes",
+                        "Tamires", "Curso de Java", "java", subCategory));
+    }
+
+    @Test
+    void getTotalCourseHoursShouldReturnTotalCourseHours() {
+        assertEquals(getTotalCourseHours(courses), 30);
+    }
+
+    @Test
+    void getSubCategoryNameShouldReturnNameSubCategoryWithBrTags() {
+        assertEquals(getSubCategoryName(courses), "java</br> Descrição: java</br></br> java</br> Descrição: java</br></br> java</br> Descrição: java</br></br>");
+    }
+
+
+}
