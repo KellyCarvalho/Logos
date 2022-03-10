e694350e9fb55f791d85a58c5f84816608f578f2
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-09 23:08:03 -0300

    cleaning imports program class

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,3 @@
-import Logos.category.Category;
-import Logos.course.Course;
-import Logos.subCategory.SubCategory;
-
-import java.util.List;
-
-import static Logos.course.CourseService.getSubCategoryName;
-import static Logos.utils.CsvReader.*;
 import static Logos.utils.GenerateSql.writeSql;
 
 public class Program {
