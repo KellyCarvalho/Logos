1c13d9125f828c18c3d51bbe377632b6d289fb2a
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-09 23:05:35 -0300

    deleting loadSql

--- src/test/java/CourseServiceTest.java
+++ src/test/java/CourseServiceTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import static Logos.course.CourseService.getSubCategoryName;
 import static Logos.course.CourseService.getTotalCourseHours;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CourseServiceTest {
 
