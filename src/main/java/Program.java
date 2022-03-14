ebfc9f5f1b16aa28178f8e80a3d4f4677bfa7143
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-14 15:38:15 -0300

    Test main

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,13 +1,12 @@
 import java.sql.SQLException;
 
-import static Logos.course.DaoCourse.delete;
-import static Logos.course.DaoCourse.turnPublic;
+import static Logos.course.DaoCourse.*;
 
 public class Program {
 
     public static void main(String[] args) throws SQLException {
-       // System.out.println(insert());
-       delete(" java-jpa-consultas3333333-avancadas-performance-modelos-complexos ");
-        System.out.println("Linhas afetadas: "+ turnPublic());
+       //System.out.println(insert());
+       //delete(" java-jpa-consultas-avancadas-performance-modelos-complexos ");
+        //System.out.println("Linhas afetadas: "+ turnPublic());
     }
 }
