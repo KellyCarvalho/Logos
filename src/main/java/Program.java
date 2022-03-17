5d70b8e447574b1d5d18ad87a94fc5ed4c48776e
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-03-17 11:39:46 -0300

    deleting jars

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -11,6 +11,7 @@ public class Program {
     public static void main(String[] args) throws SQLException {
         generateCoursePage();
 
+        //DAO
         CourseDao courseDao = new CourseDao();
 
         SubcategoryDao subcategoryDao = new SubcategoryDao();
