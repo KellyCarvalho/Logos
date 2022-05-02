f474733f69699432a287c9844e96fe0a97655d95
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-05-02 19:22:15 -0300

    fix security

--- src/main/java/br/com/logos/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/logos/config/security/SecurityConfiguration.java
@@ -31,26 +31,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-        @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/admin/**").hasRole("ADMINISTRATOR")
+                .antMatchers("/category/**").hasAnyRole("ADMINISTRATOR", "STUDENT")
+                .antMatchers("/api/categories").permitAll()
+                .antMatchers("/bGltcGEtby1jYWNoZS1kYS1hcGktYWU").permitAll()
+                .anyRequest().hasRole("ADMINISTRATOR")
+                .and()
+                    .formLogin().loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/admin/categories")
+                .and()
+                    .csrf().disable();
     }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/admin/**").hasRole("ADMINISTRATOR")
-//                .antMatchers("/category/**").hasAnyRole("ADMINISTRATOR", "STUDENT")
-//                .antMatchers("/api/categories").permitAll()
-//                .antMatchers("/bGltcGEtby1jYWNoZS1kYS1hcGktYWU").permitAll()
-//                .anyRequest().hasRole("ADMINISTRATOR")
-//                .and()
-//                    .formLogin().loginPage("/login").permitAll()
-//                    .defaultSuccessUrl("/admin/categories")
-//                .and()
-//                    .csrf().disable();
-//    }
 
     @Override
     public void configure(WebSecurity web) {
